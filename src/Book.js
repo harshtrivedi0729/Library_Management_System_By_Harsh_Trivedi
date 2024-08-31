745d8ad58bf16ee857f986572eaeb0ad78231287
harshtrivedi0729 <24carat.harsh29@gmail.com>
2024-08-31 22:25:58 +0530

    Add Book class for BorrowBook and returnBook

---
+++ src/Book.js
@@ -0,0 +1,24 @@
+class Book {
+    constructor(isbn, title, author, publicationYear) {
+      this.isbn = isbn;
+      this.title = title;
+      this.author = author;
+      this.publicationYear = publicationYear;
+      this.isAvailable = true;
+    }
+  
+    borrowBook() {
+      if (this.isAvailable) {
+        this.isAvailable = false;
+      } else {
+        throw new Error('Book is already borrowed');
+      }
+    }
+  
+    returnBook() {
+      this.isAvailable = true;
+    }
+  }
+  
+  module.exports = Book;
+  
