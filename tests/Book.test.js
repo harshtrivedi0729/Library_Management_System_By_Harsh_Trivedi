9d16637ff3e9490246c26b148c011c26a352a9a2
harshtrivedi0729 <24carat.harsh29@gmail.com>
2024-08-31 22:46:27 +0530

    Fix Tests for Book

--- tests/Book.test.js
+++ tests/Book.test.js
@@ -1,24 +1,33 @@
-class Book {
-    constructor(isbn, title, author, publicationYear) {
-      this.isbn = isbn;
-      this.title = title;
-      this.author = author;
-      this.publicationYear = publicationYear;
-      this.isAvailable = true;
-    }
-  
-    borrowBook() {
-      if (this.isAvailable) {
-        this.isAvailable = false;
-      } else {
-        throw new Error('Book is already borrowed');
-      }
-    }
-  
-    returnBook() {
-      this.isAvailable = true;
-    }
-  }
-  
-  module.exports = Book;
-  
+const Book = require('../src/Book');
+
+describe('Book', () => {
+  let book;
+
+  beforeEach(() => {
+    book = new Book('123', 'Effective JavaScript', 'David Herman', 2012);
+  });
+
+  test('should create a book with correct properties', () => {
+    expect(book.isbn).toBe('123');
+    expect(book.title).toBe('Effective JavaScript');
+    expect(book.author).toBe('David Herman');
+    expect(book.publicationYear).toBe(2012);
+    expect(book.isAvailable).toBe(true);
+  });
+
+  test('should allow borrowing a book if it is available', () => {
+    book.borrowBook();
+    expect(book.isAvailable).toBe(false);
+  });
+
+  test('should not allow borrowing a book if it is already borrowed', () => {
+    book.borrowBook();
+    expect(() => book.borrowBook()).toThrow('Book is already borrowed');
+  });
+
+  test('should allow returning a borrowed book', () => {
+    book.borrowBook();
+    book.returnBook();
+    expect(book.isAvailable).toBe(true);
+  });
+});
